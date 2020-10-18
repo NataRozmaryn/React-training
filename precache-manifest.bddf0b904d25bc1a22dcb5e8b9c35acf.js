142e80c998630ffd744bd8e2f990738d8906d89f
Nataliya Rozmarynovych <n.rozmarynovych@globallogic.com>
2020-10-18 21:58:34 +0300

    Updates

---
+++ precache-manifest.bddf0b904d25bc1a22dcb5e8b9c35acf.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2faf043a3568d977ee6fb3c754dec7b0",
+    "url": "/React-training/index.html"
+  },
+  {
+    "revision": "c2801cfd92f75c95d867",
+    "url": "/React-training/static/css/main.0cc0f8cd.chunk.css"
+  },
+  {
+    "revision": "81b9e58a48aad9591907",
+    "url": "/React-training/static/js/2.fe0b2b77.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/React-training/static/js/2.fe0b2b77.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c2801cfd92f75c95d867",
+    "url": "/React-training/static/js/main.5c34b389.chunk.js"
+  },
+  {
+    "revision": "49c4e10addc793e988cd",
+    "url": "/React-training/static/js/runtime-main.b7bf715b.js"
+  }
+]);
